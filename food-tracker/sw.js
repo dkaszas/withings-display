ea6326fd316d7f4ca665ed727e4c5947a9708511
Daniel Kaszás <kaszas@Mac.localdomain>
2026-04-12 19:54:58 +0200

    Fix: inject missing bg-gold CSS class to restore invisible side pillars

--- food-tracker/sw.js
+++ food-tracker/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'macrolens-v86';
+const CACHE_NAME = 'macrolens-v87';
 const ASSETS = [
   './',
   './index.html',
@@ -7,13 +7,13 @@ const ASSETS = [
   './manifest.json'
 ];
 
-self.addEv86entListener('install', ev86ent => {
+self.addEv87entListener('install', ev87ent => {
   self.skipWaiting();
-  ev86ent.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  ev87ent.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
-self.addEv86entListener('activ86ate', ev86ent => {
-  ev86ent.waitUntil(
+self.addEv87entListener('activ87ate', ev87ent => {
+  ev87ent.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
@@ -22,11 +22,11 @@ self.addEv86entListener('activ86ate', ev86ent => {
   );
 });
 
-self.addEv86entListener('fetch', ev86ent => {
-  ev86ent.respondWith(
-    caches.match(ev86ent.request).then(response => {
-      // Return cached v86ersion or fetch from network (Network-first strategy for API calls not implemented as we only cache static assets)
-      return response || fetch(ev86ent.request);
+self.addEv87entListener('fetch', ev87ent => {
+  ev87ent.respondWith(
+    caches.match(ev87ent.request).then(response => {
+      // Return cached v87ersion or fetch from network (Network-first strategy for API calls not implemented as we only cache static assets)
+      return response || fetch(ev87ent.request);
     })
   );
 });
